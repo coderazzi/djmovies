1a566a26ac9e3ebca495774f5ddc6e8f534613fe
coderazzi <lu@coderazzi.net>
2015-04-10 23:30:09 +0000

    Movies control: filters in place

--- movies/static/movies_control.js
+++ movies/static/movies_control.js
@@ -1,15 +1,77 @@
 function setupMoviesControl() {
-	function create_filter(){
 
+	function create_cell_filter(exact, td, val){
+		var ltd=td, lval=val;
+		if (exact) return function(tds){
+			return lval===$(tds[ltd]).text();
+		}
+		return function(tds){
+			return $(tds[ltd]).text().indexOf(lval)>=0;
+		}
 	}
-	var like_filters={
-		1 : $('#title-filter').change(create_filter), //second td in rows
+
+	function create_filter(){
+		var effective=[];
+		for (var each in filters){
+			var filter=filters[each];
+			var val = filter[0].val();
+			if (val){
+				effective.push(create_cell_filter(filter[2], filter[1], val));
+			}
+		}
+		$('table#movies_control tbody tr').each(function(){
+			var $tr=$(this);
+			var tds=$tr.children(), ok=true;
+			for (var e in effective){
+				if (!effective[e](tds)){
+					ok=false;
+					break;
+				}
+			}
+			if (ok) $tr.show(); else $tr.hide();
+			//console.log(ok);
+		});
 	}
-	var exact_filters={
-		2: $('#year-filter').change(create_filter),
-		$('#format-filter').change(create_filter),
-		$('#audios-filter').change(create_filter),
-		$('#subs-filter').change(create_filter),
-		$('#location-filter').change(create_filter),
-	};
+
+	function fill_combobox($combobox, content_class, split){
+		var ret={}
+		$(content_class).each(function(){
+			var text=$.trim($(this).text());
+			if (text){
+				if (split){
+					text.split(splitter).forEach(function(v){ret[v]=true;});
+				} else {
+					ret[text]=true;
+				}
+			}
+		});
+		$combobox.append($("<option></option>"));
+		Object.keys(ret).sort().forEach(function(v) {
+        	$combobox.append($("<option></option>").text(v));
+       });
+	}
+
+	var $title=$('#title-filter').change(create_filter);
+	var $year=$('#year-filter').change(create_filter);
+	var $format=$('#format-filter').change(create_filter);
+	var $location=$('#location-filter').change(create_filter);
+	var $audios=$('#audios-filter').change(create_filter);
+	var $subs=$('#subs-filter').change(create_filter);
+
+	var splitter=/\s*\/\s*/;	
+	var filters=[
+		[$title, 1, false],
+		[$year, 2, true],
+		[$format, 4, true],
+		[$audios, 5, false],
+		[$subs, 6, false],
+		[$location, 7, true],
+	];
+
+	fill_combobox($year, '.ic_year');
+	fill_combobox($format, '.ic_format');
+	fill_combobox($audios, '.ic_audios', true);
+	fill_combobox($subs, '.ic_subs', true);
+	fill_combobox($location, '.ic_location');
+
 }
--- movies/views/movies_control.py
+++ movies/views/movies_control.py
@@ -3,27 +3,41 @@ from django.shortcuts import render_to_response
 
 from movies.models import *
 
+# def index(request):    
+#     table, images = {}, {}
+
+#     for image in Image.objects.filter(size=Image.SIZE_BASIC):
+#         images[image.movie_id]=image.servepath()
+
+#     for movie in Movie.objects.order_by('title')[:12]:
+#         key = (movie.title, movie.year, movie.imdb_link)
+#         locations=[(each.location, each.path) for each in movie.moviepath_set.all()]
+#         table.setdefault(key, []).append((movie, locations))
+
+#     info, keys = [], table.keys()
+#     keys.sort()
+#     for k in keys:
+#         image, mapped, cnt = None, table[k], 0
+#         for m, l in  mapped:
+#             if not image: image = images.get(m.id)
+#             cnt += max(1, len(l))
+#         info.append((k, image, cnt, [(m, l, max(1, len(l))) for m, l in mapped]))
+#         if len(info)>4: break
+
+#     context = Context({
+#         'info': info,
+#     })
+#     return render_to_response('movies_control.html', context)
+
 def index(request):    
-    table, images = {}, {}
+    info, images = [], {}
 
     for image in Image.objects.filter(size=Image.SIZE_BASIC):
         images[image.movie_id]=image.servepath()
 
-    for movie in Movie.objects.order_by('title')[:12]:
-        imdb_link=movie.imdb_link
-        key = (movie.title, movie.year, imdb_link)
+    for movie in Movie.objects.order_by('title', 'year')[:12]:
         locations=[(each.location, each.path) for each in movie.moviepath_set.all()]
-        table.setdefault(key, []).append((movie, locations))
-
-    info, keys = [], table.keys()
-    keys.sort()
-    for k in keys:
-        image, mapped, cnt = None, table[k], 0
-        for m, l in  mapped:
-            if not image: image = images.get(m.id)
-            cnt += max(1, len(l))
-        info.append((k, image, cnt, [(m, l, max(1, len(l))) for m, l in mapped]))
-        if len(info)>4: break
+        info.append((movie, images.get(movie.id), locations))
 
     context = Context({
         'info': info,
