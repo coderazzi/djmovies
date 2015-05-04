8576499074e61bf7b3f1346b11bfaf6b0680667a
coderazzi <lu@coderazzi.net>
2015-05-04 18:06:59 +0000

    nc

--- movies/views/movies_control.py
+++ movies/views/movies_control.py
@@ -10,7 +10,7 @@ def index(request):
         images[image.movie_id]=image.servepath()
 
     #for movie in Movie.objects.order_by('title', 'year'):#[:21]:        
-    for movie in Movie.objects.order_by('title', 'year').filter(year__in=[1959, 1960]):
+    for movie in Movie.objects.order_by('title', 'year'):#.filter(year__in=[1959, 1960]):
         locations=[(each.location, each.path) for each in movie.moviepath_set.all()]
         info.append((movie, images.get(movie.id), locations, 
             movie.genres.split('/') if movie.genres else [],
