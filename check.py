c571978b3597f07c610f82eadb5f026db08098c6
coderazzi <lu@coderazzi.net>
2018-01-22 21:51:20 +0000

    nc

--- check.py
+++ check.py
@@ -1,9 +1,11 @@
+import os
 import re
 import subprocess
 import threading
 import time
 
 MAX_TIME_FMPEG_INFO = 30  # 30 seconds
+MAX_TIME_FMPEG = 1800  # 30 minutes
 FFMPEG_INFO_PATTERN = re.compile('^\s+Stream #(.+)$')
 FFMPEG_INFO_SPECIFIC_PATTERN = re.compile('^(\d+):(\d+)(?:\((\w+)\))?: (Video|Audio|Subtitle):(.*)$')
 LANG_BY_PRIO = ['eng', 'esp', 'ger', 'fre', 'por']
@@ -12,10 +14,25 @@ AUDIO_MODES_BY_PRIO = ['5.1', 'stereo', 'mono']
 AUDIO_FREQUENCY_PATTERN = re.compile('^\s*(\\d\\d\\d\\d\\d) Hz\s*$')
 AUDIO_RATE_PATTERN = re.compile('^\s*(\\d+) kb/s')
 
+VIDEO_EXTENSIONS = ['.mkv']
+SUBTITLE_EXTENSIONS = ['.srt']
+DISMISS_EXTENSIONS = []
+SUBTITLES_LANGUAGES = {'.en': 'eng'}
+
+
+FFMPEG_TARGET = '/Volumes/TTC/__MOVIES'  # should be part of the arguments
 
 _COMM_KILL = 1
 _COMM_FINISHED = 2
 _COMM_KILLED = 3
+
+
+def _warning(filename, message):
+    print '***W*** [', filename, '] :', message
+
+
+def _action_message(filename, action):
+    print '*  !  * [', filename, '] :', action
 
 
 def _process_killer(proc, max_time, communication):
@@ -42,29 +59,63 @@ def _shell(proc_args, max_time):
 
 
 def ffmpeg_text_info(filename):
+    subtitles = []
+    if os.path.isdir(filename):
+        video = None
+        for sub in os.listdir(filename):
+            if not sub.startswith('.'):
+                fullname = os.path.join(filename, sub)
+                if os.path.isdir(fullname):
+                    raise Exception('Found unexpected subdirectory ' + fullname)
+                elif not fullname.startswith(filename):
+                    raise Exception('Found unexpected file ' + fullname)
+                else:
+                    base, ext = os.path.splitext(sub.lower())
+                    if ext in VIDEO_EXTENSIONS:
+                        if video:
+                            raise Exception('Found multiple videos in directory ' + filename + ':' + sub + "*" + video)
+                        video = fullname
+                    elif ext in SUBTITLE_EXTENSIONS:
+                        _, language = os.path.splitext(base)   # xxx.en.srt
+                        try:
+                            lang_extension = SUBTITLES_LANGUAGES[language]  # convert .en => eng
+                        except ValueError:
+                            raise Exception('Incorrect subtitle language found: ' + fullname)
+                        if lang_extension in LANG_BY_PRIO:
+                            subtitles.append(('%d:0(%s): Subtitle: external' % (len(subtitles)+1, lang_extension),
+                                              fullname))
+                    elif ext not in DISMISS_EXTENSIONS:
+                        raise Exception('Found unexpected file: ' + fullname)
+        if not video:
+            raise Exception('No video files found in directory ' + filename)
+        filename = video
+
     _, _, err = _shell("ffmpeg -i " + filename, MAX_TIME_FMPEG_INFO)
     for line in err.splitlines():
         match = FFMPEG_INFO_PATTERN.match(line)
         if match:
-            yield match.group(1)
+            yield match.group(1), 0, filename
+    if subtitles:
+        for i, c in enumerate(subtitles):
+            yield c[0], i+1, c[1]
 
 
 def _ffmpeg_info(filename):
     sequences = []
     definitions = [sequences, [], [], []]
-    for info in ffmpeg_text_info(filename):
+    for info, input_stream, fullname in ffmpeg_text_info(filename):
         match = FFMPEG_INFO_SPECIFIC_PATTERN.match(info)
         if not match:
             raise Exception(filename + " : " + info)
         stream, sequence, language, stream_type, more = match.groups()
         if not language:
             raise Exception(filename + " : " + info + ' missing language')
-        if stream != '0':
-            raise Exception(filename + " : " + info + ' invalid stream ' + stream)
-        seq = int(sequence)
+        if int(stream) != input_stream:
+            raise Exception(filename + " : " + info + ' invalid stream ' + stream + " : expected " + input_stream)
+        seq = (input_stream, int(sequence), fullname)
         if seq in sequences:
-            raise Exception(filename + " : multiples tracks with seq number " + seq)
-        definitions[['Video', 'Audio', 'Subtitle'].index(stream_type) + 1].append((int(sequence), language, more))
+            raise Exception(filename + " : multiples tracks with seq number " + str(seq))
+        definitions[['Video', 'Audio', 'Subtitle'].index(stream_type) + 1].append((seq, language, more))
         sequences.append(seq)
     return definitions
 
@@ -86,6 +137,8 @@ def suggest(filename, do_changes):
     final_videos = _sort_tracks(video, _get_video_key)
     final_audios = _sort_tracks(audio, _get_audio_key_wrapper(filename))
     final_subs = [x for x in _sort_tracks(subtitles, _get_subtitle_key) if x[1] in LANG_BY_PRIO]
+
+    # issue warnings, if required
     if not final_subs:
         _warning(filename, 'no subtitles')
     else:
@@ -106,24 +159,59 @@ def suggest(filename, do_changes):
             _warning(filename, 'First language is not ' + LANG_BY_PRIO[0])
         if final_videos[0][1] != first_audio:
             _warning(filename, 'Video language is not ' + first_audio)
-    final_sequences = (_check_default_force(filename, final_videos, do_changes)
-                       + _check_default_force(filename, final_audios, do_changes)
-                       + _check_default_force(filename, final_subs, do_changes))
-    if final_sequences != sequences:
+
+    _correct_sequencing(sequences, final_videos + final_audios + final_subs, do_changes)
+
+    #  if there are sequences to move, do it before modifying the defualt / forced tracks
+
+
+    # final_sequences = (_correct_default_force_tracks(filename, final_videos, do_changes)
+    #                    + _correct_default_force_tracks(filename, final_audios, do_changes)
+    #                    + _correct_default_force_tracks(filename, final_subs, do_changes))
+    # print final_sequences
+    #if final_sequences != sequences:
         # ffmpeg -i Peter_Pan__2003.mkv -f srt -i Peter_Pan__2003.en.srt -map 0:0 -map 0:1 -map 1:0 -c:v copy -c:a copy -c:s srt -metadata:s:s:0 language=eng -metadata title="Peter Pan 2003" output.mkv
-        _warning(filename, " : final sequences are " + str(final_sequences))
+        #_warning(filename, " : final sequences are " + str(final_sequences))
 
 
-def _warning(filename, message):
-    print '***W*** [', filename, '] :', message
+def _correct_sequencing(original_sequences, final_tracks, do_changes):
+    if (original_sequences == [x[0] for x in final_tracks]) and (set([x[0] for x in original_sequences]) == set([0])):
+        # no need to change the sequencing
+        return None
+
+    streams = set()
+    commands = ['ffmpeg', '-n', '-hide_banner']
+    mappings = []
+    metadata = ['-c:v copy -c:a copy -c:s srt']
+
+    for track, lang, _, _ in final_tracks:
+        stream, seq, filename = track
+        if stream not in streams:
+            if stream:  # new subtitle
+                commands.append('-f')
+                commands.append('srt')
+                metadata.append('-metadata:s:s:%d' % (len(streams)-1))
+                metadata.append('language=%s' % lang)
+            commands.append('-i')
+            commands.append(filename)
+            streams.add(stream)
+        mappings.append('-map')
+        mappings.append('%d:%d' % (stream, seq))
+
+    created_file = os.path.join(FFMPEG_TARGET, original_sequences[0][2])
+    command = ' '.join(commands + mappings + metadata + [created_file])
+    print command
+    if do_changes:
+        a, b, c = _shell(command, MAX_TIME_FMPEG)
+        print a
+        print b
+        print c
+
+    return created_file
 
 
-def _action_message(filename, action):
-    print '*  !  * [', filename, '] :', action
-
-
-def _check_default_force(filename, tracks, do_changes):
-    '''mkvpropedit output.mkv --edit track:4 --set flag-default=0'''
+def _correct_default_force_tracks(filename, tracks, do_changes):
+    """mkvpropedit output.mkv --edit track:4 --set flag-default=0"""
 
     def _action(track, forced, set):
         if do_changes:
@@ -145,9 +233,9 @@ def _check_default_force(filename, tracks, do_changes):
                 msg += ' (forced) '
             else:
                 msg += ' (default) '
-            _action_message(filename, msg + ' on track ' + str(seq))
+            _action_message(seq[2], msg + ' on track ' + str(1))
 
-    first, ret = True, []
+    first = True
     for seq, _, is_default, is_forced in tracks:
         if is_forced:
             _action(seq, True, False)
@@ -157,8 +245,6 @@ def _check_default_force(filename, tracks, do_changes):
                 _action(seq, False, True)
         elif is_default:
             _action(seq, False, False)
-        ret.append(seq)
-    return ret
 
 
 def _sort_tracks(tracks, key_provider):
@@ -169,7 +255,7 @@ def _sort_tracks(tracks, key_provider):
     for _, seq, lang, info in tmp:
         is_default = '(default)' in info
         is_forced = '(forced)' in info
-        ret.append((seq, lang, is_default, is_forced))  # seq, language, is_default, is_forced
+        ret.append((seq, lang, is_default, is_forced))
     return ret
 
 
@@ -244,7 +330,7 @@ if __name__ == '__main__':
             print '\n', each
             print '='*len(each)
             for l in ffmpeg_text_info(each):
-                print l
+                print l[0]
     elif sys.argv[1] == '-s':
         for each in sys.argv[2:]:
             suggest(each, False)
