69d8b19fd828437d52856a5eaf9527a306f02fc9
coderazzi <lu@coderazzi.net>
2018-02-06 10:48:14 +0000

    Created normalized folder

--- set-language.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ "$#" -eq 3 ]]; then
-    echo "mkvpropedit $1 --edit track:$3 --set language=$2"
-    mkvpropedit $1 --edit track:$3 --set language=$2
-else
-    echo "set-language.sh filename language track"
-fi
