797ae925e5207edfd9a7169b3a182efd735ec931
coderazzi <coderazzi@gmail.com>
2025-01-08 02:34:31 +0100

    Preparation for github repository, without user data

--- go.sh
+++ go.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 #export MAGICK_HOME=/usr/local/Cellar/imagemagick@6/6.9.9-27/
 #unalias python
+
 cd `dirname $0`
 source venv/bin/activate
-xdg-open  "http://127.0.0.1:8000"
+
+if [ "$(uname -s)" = "Darwin" ] ; then
+  open  "http://127.0.0.1:8000"
+else
+  xdg-open  "http://127.0.0.1:8000"
+fi
 python3 ./manage.py runserver
